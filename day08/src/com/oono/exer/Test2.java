a3cee5339f1eb07d0acf6896d787ef49b30476b8
oono <andychao3210@gmail.com>
2020-10-05 22:59:40 +0800

    2020.10.5 commits

---
+++ day08/src/com/oono/exer/Test2.java
@@ -0,0 +1,26 @@
+package com.oono.exer;
+
+/**
+ * @author oono
+ * @date 2020 10 03
+ */
+
+public class Test2{
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+class Ticket{
+
+    private int num = 30;
+
+    public void sale(){
+        
+    }
+
+}
